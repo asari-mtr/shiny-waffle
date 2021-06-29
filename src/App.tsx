5615410caaf007901d5a38e1c33e2d671325bf58
Asari Mitsuteru <asari.mtr@gmail.com>
2021-06-29 15:05:39 +0000

    mock

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,135 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>ID:</label>3fa85f64-5717-4562-b3fc-2c963f66afa6
+      <label>Name:</label>sample remo
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Mac</th>
+            <th>Seriral</th>
+            <th>Firmware</th>
+            <th>Create</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Remo mini</td>
+            <td>cc-12-2a-46-0c-39</td>
+            <td>00000000000001</td>
+            <td>202.200000001</td>
+            <td>2021/06/29</td>
+          </tr>
+          <tr>
+            <td>Remo mini</td>
+            <td>cc-12-2a-46-0c-39</td>
+            <td>00000000000001</td>
+            <td>202.200000001</td>
+            <td>2021/06/29</td>
+          </tr>
+          <tr>
+            <td>Remo mini</td>
+            <td>cc-12-2a-46-0c-39</td>
+            <td>00000000000001</td>
+            <td>202.200000001</td>
+            <td>2021/06/29</td>
+          </tr>
+          <tr>
+            <td>Remo mini</td>
+            <td>cc-12-2a-46-0c-39</td>
+            <td>00000000000001</td>
+            <td>202.200000001</td>
+            <td>2021/06/29</td>
+          </tr>
+          <tr>
+            <td>Remo mini</td>
+            <td>cc-12-2a-46-0c-39</td>
+            <td>00000000000001</td>
+            <td>202.200000001</td>
+            <td>2021/06/29</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>State</th>
+            <th>NickName</th>
+            <th>Type</th>
+            <th>Model</th>
+            <th>Manufacture</th>
+            <th>Countory</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>t</td>
+            <td>テレビ</td>
+            <td>TV</td>
+            <td>Sharp</td>
+            <td>sharp</td>
+            <td>JA</td>
+          </tr>
+          <tr>
+            <td>t</td>
+            <td>テレビ</td>
+            <td>TV</td>
+            <td>Sharp</td>
+            <td>sharp</td>
+            <td>JA</td>
+          </tr>
+          <tr>
+            <td>t</td>
+            <td>テレビ</td>
+            <td>TV</td>
+            <td>Sharp</td>
+            <td>sharp</td>
+            <td>JA</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Value</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+         <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+          <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+          <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+          <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+          <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+          <tr>
+            <td>te</td>
+            <td>25.7</td>
+            <td>2021/06/26 20:36</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
