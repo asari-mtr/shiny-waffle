0404389df34cc519f6f94e08308362263fdf558e
Asari Mitsuteru <asari.mtr@gmail.com>
2021-06-30 14:42:36 +0000

    Use useState

--- src/NamePlate.tsx
+++ src/NamePlate.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NamePlate() {
+    const [state] = useState({
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        name: "sample remo"
+    })
     return (
         <React.Fragment>
-            <label>ID:</label>3fa85f64-5717-4562-b3fc-2c963f66afa6
-            <label>Name:</label>sample remo  
+            <label>ID:</label>{state.id}
+            <label>Name:</label>{state.name}
         </React.Fragment>
     );
 };
